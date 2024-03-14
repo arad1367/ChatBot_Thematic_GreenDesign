728d6c907a7f68aa03a6442080fa6bdff52b1476
Pejman <pejman.ebrahimi77@gmail.com>
2024-03-14 12:46:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,31 +2,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import gradio as gr
 import torch
 
-# Initialize tokenizer and model
-tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-large")
-model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-large")
 
-# Function to generate response given user input and chat history
-def generate_response(user_input, history=[]):
-    # Tokenize the new input sentence
-    new_user_input_ids = tokenizer.encode(
-        user_input + tokenizer.eos_token, return_tensors="pt"
-    )
-
-    # Append the new user input tokens to the chat history
-    bot_input_ids = torch.cat([torch.LongTensor(history), new_user_input_ids], dim=-1)
-
-    # Generate a response
-    response_ids = model.generate(
-        bot_input_ids, max_length=4000, pad_token_id=tokenizer.eos_token_id
-    ).tolist()
-
-    # Convert the response tokens to text
-    response_text = tokenizer.decode(response_ids[0], skip_special_tokens=True)
-
-    return response_text, bot_input_ids.tolist()
-
-# Define the Gradio interface
 title = "GreenBot"
 description = "A friendly chatbot - Free and easy to use"
 examples = [
@@ -37,17 +13,41 @@ examples = [
     ["What do you think about artificial intelligence?"],
 ]
 
-def chatbot_interface(input_text, chat_history=[]):
-    response_text, new_chat_history = generate_response(input_text, chat_history)
-    return response_text, new_chat_history
 
-# Launch the interface
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-large")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-large")
+
+
+def predict(input, history=[]):
+    # tokenize the new input sentence
+    new_user_input_ids = tokenizer.encode(
+        input + tokenizer.eos_token, return_tensors="pt"
+    )
+
+    # append the new user input tokens to the chat history
+    bot_input_ids = torch.cat([torch.LongTensor(history), new_user_input_ids], dim=-1)
+
+    # generate a response
+    history = model.generate(
+        bot_input_ids, max_length=4000, pad_token_id=tokenizer.eos_token_id
+    ).tolist()
+
+    # convert the tokens to text, and then split the responses into lines
+    response = tokenizer.decode(history[0]).split("<|endoftext|>")
+    # print('decoded_response-->>'+str(response))
+    response = [
+        (response[i], response[i + 1]) for i in range(0, len(response) - 1, 2)
+    ]  # convert to tuples of list
+    # print('response-->>'+str(response))
+    return response, history
+
+
 gr.Interface(
-    fn=chatbot_interface,
-    inputs=["text", "state"],
-    outputs=["text", "state"],
+    fn=predict,
     title=title,
     description=description,
     examples=examples,
+    inputs=["text", "state"],
+    outputs=["chatbot", "state"],
     theme="abidlabs/pakistan",
 ).launch()
